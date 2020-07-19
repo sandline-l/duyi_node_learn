f6d500ccd661cce5aab01487df971188dd1e1f94
unknown <1610826934@qq.com>
2020-07-19 23:11:18 +0800

    注释

--- about url.js
+++ about url.js
@@ -1,5 +1,6 @@
 const URL = require("url");
 
+// 将一个url字符串解析成对象
 // const url = URL.parse("https://nodejs.org:80/a/b/c?t=3&u=5#abc");
 // console.log(url);
 // console.log(url.searchParams.has("a"));
@@ -20,5 +21,6 @@ const obj = {
   hash: "#abc"
 };
 
+// 将一个对象, 转化为 url 字符串
 const url = URL.format(obj);
 console.log(url);
--- about util.js
+++ about util.js
@@ -8,6 +8,7 @@ const util = require("util");
 //   });
 // }
 
+// 将一个异步函数 转化为 回调模式的函数
 // const delayCallback = util.callbackify(delay);
 
 // delayCallback(500, (err, d) => {
@@ -20,6 +21,7 @@ const util = require("util");
 //   }, duration);
 // }
 
+// 将一个回调模式的函数 转化成异步的promise  , 这样就方便使用 es6 里的东西了, async await 等
 // const delay = util.promisify(delayCallBack);
 // (async () => {
 //   const r = await delay(500);
@@ -46,5 +48,5 @@ const obj2 = {
     }
   }
 };
-
-console.log(util.isDeepStrictEqual(obj1, obj2));
+// 深度严格 比较, 比较是否相等
+console.log(util.isDeepStrictEqual(obj1, obj2)); 
